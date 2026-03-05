c35b2e108a910833b3aa88c395c6577f7a27fc41
C4nerr <vershu2007@gmail.com>
2026-03-06 00:06:53 +0200

    ahyet_mogna::KP_PROG_2::UPD_ZADANIE_1_2_3_3.5

--- KP_PROG_2.cpp
+++ KP_PROG_2.cpp
@@ -1,8 +1,50 @@
+
 #include <cstdint>
 #include <fstream>
+#include <iostream>
+#include <new> //для std::nothrow щоб не було bad_alloc щоб не крашити прогу
 
 using namespace std;
+//------------------------------------------------------------------------------------------------------
 
+struct arenka {
+    uint8_t* memory; //показник на пам'ять
+    uint64_t size;
+    uint64_t offset; //показує скільки байт від початку роздано
+};
+
+
+//------------------------------------------------------------------------------------------------
+arenka * arena_new(uint64_t size) {
+    if (size == 0) {
+        return nullptr; //арена нуль без сенсу
+    }
+    auto a = new(std::nothrow) arenka; //ниє про аренка тому авто
+    if (a == nullptr) {
+        return nullptr;
+    }
+    a->memory = new uint8_t[size];
+    a->size = size;
+    a->offset = 0;
+    return a;
+}
+
+void* arena_alloc(arenka* a, uint64_t size) {
+    if (a->offset + size > a->size) { //сайз -кусок який просять виділити. а-сайз - розмір всієї арени
+        return nullptr;
+    }
+    void* result = a->memory + a->offset; //якшо місце є, запам'ятовуємо адрес з якого починається непотрібна пам'ять
+    a->offset += size;
+    return result; //віддаєм пам'ять
+}
+
+void arena_free(arenka* a) {
+    delete[] a->memory;
+    delete a;
+}
+
+
+//----------------------------------------------------------------------------------------------------
 union Pixel {
     uint32_t rgba; //доступ до всього каналу (4 байта)
     struct {
@@ -11,6 +53,9 @@ union Pixel {
 };
 
 void create_color (Pixel** pixels, uint32_t shirina, uint32_t visota) {
+    if (pixels == nullptr) {
+        return;
+    }
     for (uint32_t i = 0; i < shirina; i++) {
         for (uint32_t j = 0; j < visota; j++) {
             pixels[i][j].chan.r = i+j;
@@ -22,6 +67,12 @@ void create_color (Pixel** pixels, uint32_t shirina, uint32_t visota) {
 };
 
 void save_tga(const char* filename, uint32_t shirina, uint32_t visota, Pixel** pixels) {
+    if (pixels == nullptr || filename == nullptr) {
+        return;
+    }
+    if (shirina == 0 || visota == 0) {
+        return;
+    }
     uint8_t header[18] = {0}; //у будь якого формата є заголовок. ми його задаєм. тга коштує рівно 18 байт.
     header[2] = 2; //означає шо картінка рджб без зжатія
     header[16] = 32; //глубіна кольору
@@ -34,6 +85,9 @@ void save_tga(const char* filename, uint32_t shirina, uint32_t visota, Pixel** p
 
 
     ofstream fout(filename,ios_base::binary);
+    if (fout.is_open() == false) {
+        return;
+    }
     fout.write((char*)header, 18);
     for (uint32_t i = 0; i < visota; i++) {
         for (uint32_t j = 0; j < shirina; j++) {
@@ -42,22 +96,26 @@ void save_tga(const char* filename, uint32_t shirina, uint32_t visota, Pixel** p
     }
     fout.close();
 }
-
+//------------------------------------------------------------------------------------------
 int main(int argc, char const *argv[]) {
+    arenka* my_arenna = arena_new(3000000);
     uint32_t shirina = 700;
     uint32_t visota = 900;
-    Pixel** pix = new Pixel*[shirina];
+    auto pix = (Pixel**)arena_alloc(my_arenna, shirina*sizeof(Pixel*)); //авто шоб авоід дублікейт нейм
     for (uint32_t i = 0; i < shirina; ++i) {
-        pix[i] = new Pixel[visota];
+        pix[i] = (Pixel*)arena_alloc(my_arenna, visota*sizeof(Pixel));
+        if (pix[i] == nullptr) {
+            arena_free(my_arenna);
+            cerr << "Memory allocation failed" << endl;
+            exit(-1);
+        }
     }
-//------------------------------------------------------
+
     create_color(pix, shirina, visota);
     save_tga("kartinka.tga", shirina, visota, pix);
 
-//-------------------------------------------------------
-    for (uint32_t i = 0; i < shirina; ++i) {
-        delete[] pix[i];
-    }
-    delete[] pix;
+
+   arena_free(my_arenna);
     return 0;
 }
+//------------------------------------------------------------------------------------------
