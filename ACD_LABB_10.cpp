9e9549eb1edd8f09b49de14ef823021a904fe31c
c4nerRr <veronikashyrok2007@gmail.com>
2026-04-12 20:38:02 +0300

    ACD_LABB_10.cpp ++

--- ACD_LABB_10.cpp
+++ ACD_LABB_10.cpp
@@ -60,11 +60,20 @@ void Graf_Realise::DFS(const int startVertex) const {
 
 void Graf_Realise::ply_(const int n, const int m) {
     //неорінтований граф, тому мост працює в обидві сторони
+    if (n >= id_ || m >= id_ || n < 0 || m < 0) {
+        cout << "Помилка: Вершини " << n << " або " << m << " не існує в графі!" << endl;
+        return;
+    }
+
     neighbours_[n].push_back(m);
     neighbours_[m].push_back(n);
 }
 
 void Graf_Realise::del_(const int n, const int m) {
+    if (n >= id_ || m >= id_ || n < 0 || m < 0) {
+        cout << "Помилка: Вершини " << n << " або " << m << " не існує в графі!" << endl;
+        return;
+    }
     std::erase(neighbours_[n], m);
     std::erase(neighbours_[m], n);
 }
@@ -156,37 +165,103 @@ void Graf_Realise::DFS_helper(const int vertex, std::vector<bool> &visited) cons
     }
 }
 
-
 int main() {
+    int initial_size;
+    cout << "Скільки вершин буде у графі зі старту? -> ";
+    cin >> initial_size;
+
+    // Створюємо об'єкт правильно, передаючи початковий розмір!
+    Graf_Realise GRaF(initial_size);
     char oui = '9';
+
     do {
-        cout << "Menu:" << endl;
-        cout << " ----- " << endl;
-        cout << "1. BFS" << endl;
-        cout << "2. DFS" << endl;
-        cout << "3. ply" << endl;
-        cout << "4. del_" << endl;
-        cout << "5. ply_vertices" << endl;
-        cout << " ----- " << endl;
-        cout << "0. exit" << endl;
+        cout << "\n=== Menu ===" << endl;
+        cout << "1. Вивести граф на екран (print_graph)" << endl;
+        cout << "2. Заповнити граф ребрами (vved_)" << endl;
+        cout << "3. Додати ребро (ply_)" << endl;
+        cout << "4. Видалити ребро (del_)" << endl;
+        cout << "5. Додати пусту вершину (ply_vertices)" << endl;
+        cout << "6. Видалити вершину (del_vertices)" << endl;
+        cout << "7. Обхід в ширину (BFS)" << endl;
+        cout << "8. Обхід в глибину (DFS)" << endl;
+        cout << "9. Знайти компоненти зв'язності (indiv_)" << endl;
+        cout << "0. Вихід" << endl;
+        cout << "Ваш вибір -> ";
 
         cin >> oui;
         cin.ignore();
+
         switch (oui) {
             case '1':
+                GRaF.print_graph();
                 break;
-                case '2':
+
+            case '2':
+                GRaF.vved_();
                 break;
-                case '3':
+
+            case '3': {
+                int u, v;
+                cout << "Введіть дві вершини для з'єднання (через пробіл) -> ";
+                cin >> u >> v;
+                GRaF.ply_(u, v);
+                cout << "Ребро додано!" << endl;
                 break;
-                case '4':
+            }
+
+            case '4': {
+                int u, v;
+                cout << "Введіть дві вершини для роз'єднання (через пробіл) -> ";
+                cin >> u >> v;
+                GRaF.del_(u, v);
+                cout << "Ребро видалено!" << endl;
                 break;
-                case '5':
+            }
+
+            case '5':
+                GRaF.ply_vertices_();
+                cout << "Нову порожню вершину додано в кінець!" << endl;
                 break;
-                case '0':
+
+            case '6': {
+                int v;
+                cout << "Введіть вершину для ВИДАЛЕННЯ -> ";
+                cin >> v;
+                GRaF.del_vertices_(v);
+                cout << "Вершину видалено, індекси зсунуто!" << endl;
+                break;
+            }
+
+            case '7': {
+                int start;
+                cout << "Стартова вершина для BFS -> ";
+                cin >> start;
+                GRaF.BFS(start);
+                break;
+            }
+
+            case '8': {
+                int start;
+                cout << "Стартова вершина для DFS -> ";
+                cin >> start;
+                GRaF.DFS(start);
+                break;
+            }
+
+            case '9':
+                GRaF.indiv_();
+                break;
+
+            case '0':
+
+                break;
+
+            default:
+                cout << "Невідома команда. Спробуй ще раз." << endl;
                 break;
         }
 
-    }while (oui != '0');
+    } while (oui != '0');
+
     return 0;
 }
